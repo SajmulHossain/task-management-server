04e23661d35f1abcdd33d898c8cc5715637545df
Sajmul <sajmul1427@gmail.com>
2025-02-20 23:34:03 +0600

    initial setup done and server is succesfully running

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Task management server is running!");
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
